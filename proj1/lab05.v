921cc23b50ace06d08ed034258c30d09328716ce
mchen77 <77766094+mchen77@users.noreply.github.com>
2021-03-14 13:24:04 -0400

    initalized rom and pll (10MHz clk)

--- proj1/lab05.v
+++ proj1/lab05.v
@@ -4,16 +4,19 @@
 // main module for RISC-V processor
 
 module lab05(
-	input CLOCK_50,
-	input [31:0] instr,
-	output [31:0] Y	// debug
+	input CLOCK_50	
+	
 );
 	// input clk, wren; //clock and write enable ports
 	// input [5:0] rr1, rr2, wr; //5 bit register address inputs
 	// input [31:0] wd; //data to write
 	// output [31:0] rd1, rd2; //read data outputs
-
-
+	wire [31:0] instr;
+	reg clk;
+	reg [7:0] PC; 
+	
+	initial PC = 0;
+	
 	// control_unit
 	wire [6:0] form_code = instr[6:0];
 	wire [1:0] aluop;	// 2-bits opcode to determine format (R,S, B-type)
@@ -27,7 +30,7 @@ module lab05(
 	wire [31:0] A;
 	wire [31:0] B;
 	wire zero;			// zero flag for branching
-	// wire [31:0] Y; // commented out for debug
+	wire [31:0] Y; // commented out for debug
 
 	// reg_file
 	wire [4:0] rr1 = instr[19:15];
@@ -49,14 +52,19 @@ module lab05(
 	control_unit ctrl (form_code, aluop, Branch, MemRead, MemtoReg,
 						MemWrite, ALUSrc, RegWrite);
 
-	reg_file rf (CLOCK_50, RegWrite, Y, rr1, rr2, wr, rd1, rd2);
+	reg_file rf (clk, RegWrite, Y, rr1, rr2, wr, rd1, rd2);
 
 	alu_control alu_ctrl(instr_split, aluop, aluopcode);
 
 	alu a1 (Y, zero, A, B, aluopcode);
 
 	//COMMENT OUT FOR TESTBENCH
-	//lab5_ram ram(mem_addr, CLOCK_50, rd2, MemWrite, mem_dout);
+	lab5_ram ram(mem_addr, clk, rd2, MemWrite, mem_dout);
+	
+	rom_lab5 rom(PC, clk, instr);
+	
+	// PLL
+	pll_lab5 pll(CLOCK_50, 1'b0, clk);
 
 
 	// ALU logic
@@ -64,5 +72,10 @@ module lab05(
 	assign B = (ALUSrc) ? out : rd2;	// second input to ALU, (1 imm_gen, 0 rd2)
 
 	assign wd = (MemtoReg) ? mem_dout : Y;
+	
+	always @(posedge clk) begin
+		if (PC < 8'd9) PC <= PC + 1;
+	end
+	
 
 endmodule
---
+++ proj1/pll_lab5_sim.f
@@ -0,0 +1,1 @@
+pll_lab5_sim/pll_lab5.vo
